702a20316146624bf0267cb93216152afd19e68b
Javier Larroca <javier-larroca@hotmail.com>
2025-09-10 19:45:58 -0300

    Practia realizada - Unidad 1

---
+++ primer-ejemplo/ejemplo-1-net9/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using ejemplo_1_net9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ejemplo_1_net9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Bienvenida()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ primer-ejemplo/ejemplo-2-netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ejemplo_2_netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
