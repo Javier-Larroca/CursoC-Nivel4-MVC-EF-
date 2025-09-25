eabb5b831c75cfff006eab986f6b0f23ab15de5e
Javier Larroca <javier-larroca@hotmail.com>
2025-09-24 21:04:45 -0300

    Avance Unidad 2 - Video 3

---
+++ Unidad 2 - Razor/ejemplos-asp-webforms-main/pokedex-mvc/Controllers/PruebaControllerVacio.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pokedex_mvc.Controllers
+{
+    public class PruebaControllerVacio : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
